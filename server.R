18e9b3338685eb8e03979a77530027bc2a42c7a2
Veerasak Kritsanapraphan <veerasak.kritsanapraphan@gmail.com>
2014-06-24 06:12:09 +0700

    completed

--- server.R
+++ server.R
@@ -1,6 +1,19 @@
-library(shiny) 
+library(shiny)
+library(datasets)
 
-shinyServer(
-    function(input, output) {
-    } 
-)
+preMPG <- function(wt, qsec, am) -3.1855*wt+1.5998*qsec+4.2995*am  
+shinyServer(function(input, output) {
+    
+    # sorted columns are colored now because CSS are attached to them
+    output$mytable2 <- renderDataTable({
+        library(ggplot2)
+        mtcars
+    }, options = list(bSortClasses = TRUE,aLengthMenu = c(5, 32),iDisplayLength = 5))
+    
+    # Generate the predicted value of mpg
+    output$prediction <- renderPrint({
+        preMPG(input$wt,input$qsec,input$am)})
+    
+    # Enter the value of explanatory variables
+    output$inputValue <- renderPrint({c(input$wt,input$qsec,input$am)})
+})
--- ui.R
+++ ui.R
@@ -1,13 +1,35 @@
-library(shiny) 
+library(shiny)
+library(ggplot2)  
 
-shinyUI(pageWithSidebar( 
-    headerPanel("Illustrating inputs"), 
-    sidebarPanel(
-        numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1), 
-        checkboxGroupInput("id2", "Checkbox",
-                           c("Value 1" = "1", "Value 2" = "2", "Value 3" = "3")),                                                                              c("Value 1" = "1", "Value 2" = "2", "Value 3" = "3")),
-        dateInput("date", "Date:") 
-    ),
-    mainPanel(
-    ) 
-))
+shinyUI(fluidPage(
+    title = 'Fuel Consumption Prediction',
+    sidebarLayout(
+        sidebarPanel(
+            h4('Specification:'),
+            conditionalPanel(
+                'input.dataset === "mtcars"',
+                helpText('1.View the Dataset: Motor Trend Car Road Tests'),        
+                helpText('- Click the column header to sort a column.'),
+                helpText('- Change the number of records in display.'),
+                helpText('2.Enter the Car Attributes'),
+                helpText('3.Press "Submit" to forecast the MPG')
+            ),
+            numericInput('wt', 'Weight (lb/1000) Min:3 Max:6', 3, min = 1, max = 6, step=0.01),
+            numericInput('qsec', '1/4 mile time (sec) Min:15 Max:20', 17, min = 15, max = 20, step=0.01),
+            numericInput('am', 'Transmission (0 = automatic, 1 = manual)', 1, min = 0, max = 1, step=1),
+            submitButton('Submit')
+        ),
+        mainPanel(
+            h4("Dataset: Motor Trend Car Road Tests"),
+            tabsetPanel(
+                id = 'dataset',
+                tabPanel('mtcars', dataTableOutput('mytable2'))),
+            h5("The Atrribute Value You Entered: Weight, 1/4 mile time, Transmission"),
+            verbatimTextOutput("inputValue"),
+            h5("Predict Fuel Consumption Based on Motor Trend Car Road Tests"),
+            verbatimTextOutput("prediction")
+            
+        )
+    )
+)
+)
